8d605b2fee904c9233a8f36f0371d2e45b0be8cf
AhmedRabieG <ahmedrabie010@gmail.com>
2025-02-25 17:30:34 +0200

    anma slider and reobox slide , edit nav

--- js/main.js
+++ js/main.js
@@ -17,7 +17,55 @@ const swiper = new Swiper('.swiper-container__first', {
     loop: false,
 });
 
-
+const anmaSwiper = new Swiper('.swiper-container__anma',{
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    loop: false,
+    
+})
+const programsSwiper = new Swiper('.swiper-anma-programs', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    speed: 400,
+    autoplay: {
+      delay: 3000,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    loop: true,
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 32,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 32,
+        },
+    }
+});
 const swiperTest = new Swiper('.swiper-container__Test', {
     slidesPerView: 4,
     spaceBetween: 20,
@@ -32,3 +80,66 @@ const swiperTest = new Swiper('.swiper-container__Test', {
     },
     loop: false,
 });
+
+// const roboxSwiber = new Swiper('.robox-swiper', {
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+//     speed: 400,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//     loop: false,
+// });
+
+const varietyMenu = [
+  {
+    title: 'Schedules Management',
+  },
+  {
+    title: 'Excuses'
+  },
+  {
+    title: 'Vacations'
+  },
+  {
+    title: 'Extra-Services'
+  },
+  {
+    title: 'Updating Attendance'
+  },
+  {
+    title: 'Attendance Verification '
+  },
+  {
+    title: 'Absent & Late Statements'
+  }
+
+]
+
+const roboxSwiber = new Swiper('.robox-swiper', {
+  slidesPerView: 5,
+  loop: true,
+  speed: 1500,
+  parallax: true,
+  effect: 'fade',
+  fadeEffect: { crossFade: true },
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.robox-swiper__pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return ' <li class="whitespace-nowrap ' + className + '">' +
+       varietyMenu[index]?.title + '</li>'
+    }
+  }
+})
+
+console.log(varietyMenu)
